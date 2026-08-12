7a33eda6ebc41c1a59b48eeacc770907cdbdf448
Anastasia Pletser <nastusya89pl@gmail.com>
2026-08-12 15:51:31 +0300

    configure graphql api url

--- client/src/apollo/client.js
+++ client/src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5001/graphql",
+  uri: `${process.env.REACT_APP_API_URL}/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
